d7ac6d8234c27d665af4d668578ccfc1de8b833a
Myles <kumyles@gmail.com>
2025-04-06 13:12:20 -0400

    Efficient fault tolerant broadcast finished

--- broadcast.py
+++ broadcast.py
@@ -1,17 +1,52 @@
 #!/usr/bin/env python3
 
 from maelstrom import Body, Node, Request
+import asyncio
+from collections import defaultdict
 
 if __name__ == "__main__":
     node = Node()
     messages = set()
-    neighbors: list[str] = []
+    neighbors: dict[str, list[str]] = {}
+    backlog: dict[str, list[str]] = defaultdict(list)
+
+
+    async def send_propogation_request(message, neighbor):
+        backlog[neighbor].append(message)
+        response = await node.rpc(neighbor, {"type": "propogate", "messages": backlog[neighbor]})
+        if response.get("type") != "error":
+            backlog[neighbor] = []
+
+
+        
+    @node.handler
+    async def propogate(req: Request) -> Body:
+        req_messages = req.body["messages"]
+
+        for message in req_messages:
+            if message in messages:
+                  continue
+            messages.add(message)
+            await asyncio.gather(
+            *[
+                send_propogation_request(message, neighbor)
+                for neighbor in neighbors.get(node.node_id, []) if neighbor != req.src
+            ]
+            )
+        return {"type": "propogate_ok"}
+
 
     @node.handler
     async def broadcast(req: Request) -> Body:
-        messages.add(req.body["message"])
-        for neighbor in neighbors:
-            await node.rpc(neighbor, {"type": "broadcast", "message": req.body["message"]})
+        message = req.body["message"]
+        if message not in messages:
+            messages.add(message)
+            await asyncio.gather(
+            *[
+                send_propogation_request(message, neighbor)
+                for neighbor in neighbors.get(node.node_id, []) if neighbor != req.src
+            ]
+            )
         return {"type": "broadcast_ok"}
 
     @node.handler
@@ -21,7 +56,7 @@ if __name__ == "__main__":
     @node.handler
     async def topology(req: Request) -> Body:
         global neighbors
-        neighbors = req.body["topology"].get(node.node_id, [])
+        neighbors = req.body["topology"]
         return {"type": "topology_ok"}
     
     node.run()
