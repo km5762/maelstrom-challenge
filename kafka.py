0ae9136e6603fd7b3fa26f9b04b8949d27d6b00c
km5762 <kumyles@gmail.com>
2025-05-26 16:34:33 -0400

    challenge 5 done

--- kafka.py
+++ kafka.py
@@ -6,28 +6,48 @@ from collections import defaultdict
 
 if __name__ == "__main__":
     node = Node()
-    logs: dict[str, list[int]] = defaultdict(list)
-    committed_offsets: dict[str, int] = defaultdict(int)
+
+    async def write(key, value):
+        await node.rpc("lin-kv", {"type": "write", "key": key, "value": value})
+
+    async def read(key):
+        response = await node.rpc("lin-kv", {"type": "read", "key": key})
+        return response.get("value")
+
+    async def cas(key, old, new):
+        response = await node.rpc("lin-kv", {"type":"cas", "key":key, "from":old, "to":new})
+        return response.get("type") == "cas_ok" or ((response.get("type") == "error") and (response.get("code") == 20))
+
 
     @node.handler
     async def send(req: Request) -> Body:
         key = req.body["key"]
         msg = req.body["msg"]
-        offset = len(logs[key])
-        logs[key].append(msg)
-        return {"type": "send_ok", "offset": offset}
 
+        for _ in range(0, 100):
+            old = await read(key)
+
+            if old is None:
+                await write(key, [])
+
+            new = (old or []) + [msg]
+
+            if (await cas(key, old, new)):
+                offset = len(old) if old else 0
+                return {"type": "send_ok", "offset": offset}
+
+        return {"type": "send_error", "message": "CAS failed too many times"}
+    
     @node.handler
     async def poll(req: Request) -> Body:
         offsets = req.body["offsets"]
-        msgs = {}
+        keys = list(offsets.keys())
+        logs = await asyncio.gather(*(read(key) for key in keys))
 
-        for key, offset in offsets.items():
-            if key not in msgs:
-                msgs[key] = []
-            existing_msgs = logs[key]
-            for i in range(offset, len(existing_msgs)):
-                msgs[key].append([i, existing_msgs[i]])
+        msgs = {}
+        for key, log, offset in zip(keys, logs, offsets.values()):
+            log = log or []
+            msgs[key] = [[i, log[i]] for i in range(offset, len(log))]
 
         return {"type": "poll_ok", "msgs": msgs}
 
@@ -35,18 +55,17 @@ if __name__ == "__main__":
     async def commit_offsets(req: Request) -> Body:
         offsets = req.body["offsets"]
         for key, offset in offsets.items():
-            committed_offsets[key] = offset
+            await write(key + "-offset", offset)
 
         return {"type": "commit_offsets_ok"}
 
     @node.handler
     async def list_committed_offsets(req: Request) -> Body:
         keys = req.body["keys"]
-        offsets = {}
-
-        for key in keys:
-            offsets[key] = committed_offsets[key]
-
-        return {"type": "list_committed_offsets_ok", "offsets": offsets}
+        results = await asyncio.gather(*(read(key + "-offset") for key in keys))
+        return {
+            "type": "list_committed_offsets_ok",
+            "offsets": {key: res or 0 for key, res in zip(keys, results)}
+        }
 
     node.run()
